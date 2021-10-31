702eb74f75d53a196b39a5a8d366f3bcafc658aa
Yurii Lysytsia <developer.yurii.lysytsia@gmail.com>
2021-10-31 14:40:57 +0200

    [1.0.0] Added manager

---
+++ SoundModeManager/SoundModeManager.h
@@ -0,0 +1,9 @@
+// Copyright © 2021 Yurii Lysytsia. All rights reserved.
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SoundModeManager.
+FOUNDATION_EXPORT double SoundModeManagerVersionNumber;
+
+//! Project version string for SoundModeManager.
+FOUNDATION_EXPORT const unsigned char SoundModeManagerVersionString[];
